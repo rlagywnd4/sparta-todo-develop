cc7a650d88f9d89824e87a54a6736a7a88af8873
rlagywnd4 <rlagywnd4@gmail.com>
2025-01-02 19:28:46 +0900

    feat: 유효성 검사 추가

--- sparta-todo-develop/src/main/java/com/example/sparta_todo_develop/dto/user/UserRequestDto.java
+++ sparta-todo-develop/src/main/java/com/example/sparta_todo_develop/dto/user/UserRequestDto.java
@@ -1,10 +1,17 @@
 package com.example.sparta_todo_develop.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UserRequestDto {
     private String name;
+
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "비밀번호는 최소 8자 이상이어야 하며, 영문자, 숫자, 특수 문자(@$!%*?&) 각각 최소 하나씩 포함해야 합니다.")
     private String password;
+
+    @Email
     private String email;
 }
