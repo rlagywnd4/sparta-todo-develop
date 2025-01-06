4650fba9da7ca8210b936facceb665b8a0b963ff
rlagywnd4 <rlagywnd4@gmail.com>
2025-01-06 09:48:15 +0900

    feat: 예외 처리

--- sparta-todo-develop/src/main/java/com/example/sparta_todo_develop/repository/UserRepository.java
+++ sparta-todo-develop/src/main/java/com/example/sparta_todo_develop/repository/UserRepository.java
@@ -1,19 +1,19 @@
 package com.example.sparta_todo_develop.repository;
 
 import com.example.sparta_todo_develop.entity.User;
+import com.example.sparta_todo_develop.exception.UserNotFoundException;
+import com.example.sparta_todo_develop.exception.error.ErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     default User findByIdOrElseThrow(Long id) {
-        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id));
+        return findById(id).orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND_BY_ID));
     }
 
     default User findByEmailOrElseThrow(String email) {
-        return findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist email = " + email));
+        return findByEmail(email).orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND_BY_EMAIL));
     }
 
     Optional<User> findByEmail(String email);
